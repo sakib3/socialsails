cb02a68719cc884c4642a0a516809de8037ef94d
sakib3 <sakib3@gmail.com>
2015-11-14 17:30:07 +0100

    through grunt automatically inject the js file ref in index.html

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,15 +1,25 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var mainBowerfiles = require('main-bower-files');
+var inject = require('gulp-inject');
 var paths ={
 	temp: 'temp',
+	tempVendor: 'temp/vendor',
 	index: 'app/index.html'
 };
-gulp.task('default',['tempSetup','serve']);
+gulp.task('default',['scripts','serve']);
 
-gulp.task('tempSetup',function(){
+
+gulp.task('scripts',function(){
+
 	//acces the index.html  in app dir  and copy to tmp dir
-	gulp.src(paths.index)
-	.pipe(gulp.dest(paths.temp));
+	var tempIndex = gulp.src(paths.index).pipe(gulp.dest(paths.temp));
+	//copy main files from bower components
+	var temVendors = gulp.src(mainBowerfiles()).pipe(gulp.dest(paths.tempVendor));
+
+	tempIndex
+			.pipe(inject(temVendors,{relative: true}))
+			.pipe(gulp.dest(paths.temp));
 });
 
 gulp.task('serve', function(){
