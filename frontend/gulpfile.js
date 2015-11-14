c5ee50442402e2ee12607e9cb0b4d080179ea2b1
sakib3 <sakib3@gmail.com>
2015-11-14 16:09:23 +0100

    series of gulp task but those are async so need to find out a better solution

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,12 +1,20 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
-gulp.task('default',function(){
-	//acces the index.html  in app dir  and copy to tmp dir
-	gulp.src('app/index.html')
-	.pipe(gulp.dest('temp'));
+var paths ={
+	temp: 'temp',
+	index: 'app/index.html'
+};
+gulp.task('default',['tempSetup','serve']);
 
+gulp.task('tempSetup',function(){
+	//acces the index.html  in app dir  and copy to tmp dir
+	gulp.src(paths.index)
+	.pipe(gulp.dest(paths.temp));
+});
+
+gulp.task('serve', function(){
 	//serve the index.html file
-	gulp.src('temp')
+	gulp.src(paths.temp)
 	.pipe(webserver({
 		//open the directory and show the list
 		open:true
