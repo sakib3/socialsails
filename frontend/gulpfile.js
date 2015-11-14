fbabcd6a3454475ba3693c93c721e34bb9b0dbef
sakib3 <sakib3@gmail.com>
2015-11-14 15:50:18 +0100

    gulp listing dir

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,4 +1,11 @@
 var gulp = require('gulp');
+var webserver = require('gulp-webserver');
 gulp.task('default',function(){
-	console.log("hello from gulp");
-})
+	//acces the root dir to our webserver functionality
+	gulp.src('root')
+	.pipe(webserver({
+		//open the directory and show the list
+		open:true,
+		directoryListing: true
+	}));
+});
