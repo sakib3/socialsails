636d221534bce8593a5d13dce03d66ea5dd1c606
sakib3 <sakib3@gmail.com>
2015-11-14 16:01:49 +0100

    frontend served from temp/index.html using gulp

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 gulp.task('default',function(){
-	//acces the root dir to our webserver functionality
-	gulp.src('root')
+	//acces the index.html  in app dir  and copy to tmp dir
+	gulp.src('app/index.html')
+	.pipe(gulp.dest('temp'));
+
+	//serve the index.html file
+	gulp.src('temp')
 	.pipe(webserver({
 		//open the directory and show the list
-		open:true,
-		directoryListing: true
+		open:true
+		//directoryListing: true
 	}));
 });
