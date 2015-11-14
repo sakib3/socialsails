976f6be57c869cc707e6be573cf73903260d3c30
sakib3 <sakib3@gmail.com>
2015-11-14 19:19:06 +0100

    based on watch gulp task is executed

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -2,42 +2,66 @@ var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var mainBowerfiles = require('main-bower-files');
 var inject = require('gulp-inject');
+//var del = require('del');
 var paths ={
 	temp: 'temp',
 	tempVendor: 'temp/vendor',
+	tempIndex: 'temp/index.html',
+
 	index: 'app/index.html',
-	app : 'app/**/*.js',
-	bower: 'bower_components'
+	appSrc : ['app/**/*', '!app/index.html'],
+	bowerSrc: 'bower_components/**/*'
 };
-gulp.task('default',['scripts','serve','watch']);
 
+gulp.task('default',['watch']);
 
-gulp.task('scripts',function(){
+gulp.task('watch',['serve'],function(){
+	gulp.watch(paths.appSrc, ['scripts']);
+	gulp.watch(paths.bowerSrc, ['vendors']);
+	gulp.watch(paths.index, ['copyAll']);
+});
 
-	//acces the index.html  in app dir  and copy to tmp dir
-	var tempIndex = gulp.src(paths.index).pipe(gulp.dest(paths.temp));
-	//acces all js files and folders  in app dir  and copy to tmp dir
-	var scripts = gulp.src(paths.app).pipe(gulp.dest(paths.temp));
-	//copy main files from bower components
-	var temVendors = gulp.src(mainBowerfiles()).pipe(gulp.dest(paths.tempVendor));
+gulp.task('serve', ['copyAll'], function(){
+	//serve the index.html file
+	return gulp.src(paths.temp)
+		.pipe(webserver({
+			livereload: true,
+			proxies: [{
+				source: '/api',
+				target: 'http://localhost:1337'
+		}]
+		}));
+});
 
-	tempIndex
-			.pipe(inject(scripts,{relative: true}))
-			.pipe(inject(temVendors,{relative: true, name:'vendorInject'}))
+gulp.task('copyAll',function(){
+	var tempVendors = gulp.src(mainBowerfiles()).pipe(gulp.dest(paths.tempVendor));
+	var appFiles = gulp.src(paths.appSrc).pipe(gulp.dest(paths.temp));
+	
+	return gulp.src(paths.index)
+			.pipe(gulp.dest(paths.temp))
+			.pipe(inject(tempVendors,{relative: true, name:'vendorInject'}))
+			.pipe(inject(appFiles,{relative: true}))
 			.pipe(gulp.dest(paths.temp));
 });
 
-gulp.task('watch',function(){
-	gulp.watch(paths.app, ['scripts']);
-	gulp.watch(paths.bower, ['scripts']);
+gulp.task('vendors',function(){
+	var tempVendors = gulp.src(mainBowerfiles()).pipe(gulp.dest(paths.tempVendor));
+	
+	
+	return gulp.src(paths.tempIndex)
+			.pipe(inject(tempVendors,{relative: true, name:'vendorInject'}))
+			.pipe(gulp.dest(paths.temp));
 });
-gulp.task('serve', function(){
-	//serve the index.html file
-	gulp.src(paths.temp)
-	.pipe(webserver({
-		//open the directory and show the list
-		//open:true
-		//directoryListing: true
-		livereload:true
-	}));
+
+gulp.task('scripts',function(){
+
+	var appFiles = gulp.src(paths.appSrc).pipe(gulp.dest(paths.temp));
+	return gulp.src(paths.tempIndex)
+			.pipe(inject(appFiles,{relative: true}))
+			.pipe(gulp.dest(paths.temp));
+
 });
+
+//gulp.task('clean', function(){
+//	del([paths.temp]);
+//});
