936967b1db3eb56a3f790fdf194019e981ce355e
sakib3 <sakib3@gmail.com>
2015-11-15 17:10:42 +0100

    Date time is ready for the backend

---
+++ frontend/app/scheduler.js
@@ -0,0 +1,34 @@
+angular.module("app").controller("Scheduler",function($scope,$auth,$http){
+	
+	
+	
+	$scope.tweet = function(){
+
+		var datetime = new Date(
+			$scope.date.getFullYear(),
+			$scope.date.getMonth(),
+			$scope.date.getDate(),
+			$scope.date.getHours(),
+			$scope.date.getMinutes()
+		);
+		$http.post('/api/post/tweet',{
+				message: $scope.message,
+				datetime: datetime
+		})
+		.then(function(){
+					$scope.message = '';
+		});
+	}
+	
+	$scope.time = new Date();
+
+	$scope.minDate = new Date();
+	
+	$scope.opened = false;
+	
+	$scope.open = function($event){
+		$event.preventDefault();
+		$event.stopPropagation();
+		$scope.opened = !$scope.opened;
+	}
+})
--- frontend/temp/login.js
+++ frontend/temp/login.js
@@ -1,23 +1,11 @@
 angular.module("app").controller("Login",function($scope,$auth,$http){
+	$scope.isAuthenticated = $auth.isAuthenticated;
+	
 	$scope.login = function(){
 		$auth.authenticate('twitter');
 	}
-	$scope.isAuthenticated = $auth.isAuthenticated;
-	$scope.tweet = function(){
-		$http.post('/api/post/tweet',{message: $scope.message}).then(function(){
-			$scope.message = '';
-		});
-	}
+	
 	$scope.logout = function () {
 		$auth.logout();
-	}
-
-	$scope.minDate = new Date();
-	$scope.time = new Date();
-	$scope.opened = false;
-	$scope.open = function($event){
-		$event.preventDefault();
-		$event.stopPropagation();
-		$scope.opened = !$scope.opened;
 	}
 })
