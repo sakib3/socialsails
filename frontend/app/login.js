5118a6e4fe0d7825d082a0737d6675a67d97d4be
sakib3 <sakib3@gmail.com>
2015-11-15 13:19:00 +0100

    Setting up nav with ng

--- frontend/app/login.js
+++ frontend/app/login.js
@@ -2,6 +2,7 @@ angular.module("app").controller("Login",function($scope,$auth,$http){
 	$scope.login = function(){
 		$auth.authenticate('twitter');
 	}
+	$scope.isAuthenticated = $auth.isAuthenticated;
 	$scope.tweet = function(){
 		$http.post('/api/post/tweet','').then(function(){
 
