4ba10a3104339fbee7a2b439f75eeddc97b8c8aa
sakib3 <sakib3@gmail.com>
2015-11-16 23:13:29 +0100

    Populating user data from the Tweet

--- backend/api/controllers/PostController.js
+++ backend/api/controllers/PostController.js
@@ -19,20 +19,7 @@ module.exports = {
 			}).exec(function(err,post){
 				console.log("working",post,err);
 				res.send(200).end();
-			})
-/*			var T = new Twit({
-			    consumer_key:         config.TWITTER_KEY, 
-			    consumer_secret:      config.TWITTER_SECRET, 
-			    access_token:         user.twitterToken, 
-			    access_token_secret:  user.twitterSecret
-			});
-			T.post('statuses/update', { 
-						status: message 
-					}, function(err, data, response) {
-			  			console.log(data,err);
-			  			res.status(200).end();
-			});*/
-			
+			})			
 		})
 	},
 	myPosts: function(req,res){
--- backend/scheduler.js
+++ backend/scheduler.js
@@ -1,11 +1,36 @@
-var sails = require('sails'); 
+var sails = require('sails');
+var Twit = require('twit'); 
 sails.load(function(){
 	checkPosts();
 })
 function checkPosts(){
-	Post.find().where({scheduledfor:{'<' : new Date()}}).exec(function(err,posts){
-		console.log(posts);
+	Post.find()
+	.where({
+		scheduledfor:  {'<' : new Date()}
 	})
+	.populate('owner')
+	.exec(function(err,posts){
+		console.log(posts);
+		posts.forEach(function(post){
+			//sendTweet(post.message)
+
+			
+		})
+	})
+}
+function sendTweet(token,secret,message){
+			var T = new Twit({
+			    consumer_key:         config.TWITTER_KEY, 
+			    consumer_secret:      config.TWITTER_SECRET, 
+			    access_token:         user.twitterToken, 
+			    access_token_secret:  user.twitterSecret
+			});
+			// T.post('statuses/update', { 
+			// 			status: message 
+			// 		}, function(err, data, response) {
+			//   			console.log(data,err);
+			//   			res.status(200).end();
+			// });
 }
 // setInterval(function(){
 // 	console.log("I am the scheduler!");
